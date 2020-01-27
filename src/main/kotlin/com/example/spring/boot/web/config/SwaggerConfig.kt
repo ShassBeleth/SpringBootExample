7e993be91ba8debaa1813923c85316652a4f90bb
ShassBeleth <shassbeleth@gmail.com>
2020-01-27 17:10:24 +0900

    Spring Foxわかるところを書いた

--- src/main/kotlin/com/example/spring/boot/web/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/spring/boot/web/config/SwaggerConfig.kt
@@ -3,8 +3,12 @@ package com.example.spring.boot.web.config
 import com.example.spring.boot.web.annotation.IgnoreSwaggerDocument
 import com.example.spring.boot.web.controller.spring.fox.example.ignore.IgnorePackageController
 import com.fasterxml.classmate.TypeResolver
+import com.google.common.base.Optional
 import com.google.common.base.Predicates.not
 import com.google.common.collect.Lists
+import com.google.common.collect.Maps.newHashMap
+import com.google.common.collect.Sets.newHashSet
+import io.swagger.annotations.ApiModel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -12,17 +16,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.context.request.async.DeferredResult
-import springfox.documentation.builders.ParameterBuilder
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.builders.ResponseMessageBuilder
+import springfox.documentation.builders.*
+import springfox.documentation.builders.PathSelectors.regex
 import springfox.documentation.schema.AlternateTypeRules
 import springfox.documentation.schema.ModelRef
 import springfox.documentation.schema.WildcardType
-import springfox.documentation.service.ApiKey
-import springfox.documentation.service.AuthorizationScope
-import springfox.documentation.service.SecurityReference
-import springfox.documentation.service.Tag
+import springfox.documentation.service.*
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
@@ -48,8 +47,82 @@ class SwaggerConfig {
     @Value("\${server.servlet.context-path:/}")
     private val contextPath : String? = null
 
+    /**
+     *　Getリクエストの共通レスポンス
+     */
+    private fun getGlobalGetResponses() : List<ResponseMessage> = Lists.newArrayList(
+            ResponseMessageBuilder()
+                    .code(500)
+                    .message("Global 500 Error Message")
+                    // TODO ModelRefではModelsに定義されているものなら参照可能か？
+                    .responseModel(ModelRef("SampleResponse"))
+                    // TODO ヘッダつけられないか？
+                    .build(),
+            ResponseMessageBuilder()
+                    .code(400)
+                    .message("Global 400 Error Message")
+                    .responseModel(ModelRef("SampleResponse"))
+                    .build()
+    )
+    /**
+     * Deleteリクエストの共通レスポンス
+     */
+    private fun getGlobalDeleteResponses() : List<ResponseMessage> = ArrayList()
+    /**
+     * Headリクエストの共通レスポンス
+     */
+    private fun getGlobalHeadResponses() : List<ResponseMessage> = ArrayList()
+    /**
+     * Optionsリクエストの共通レスポンス
+     */
+    private fun getGlobalOptionsResponses() : List<ResponseMessage> = ArrayList()
+    /**
+     * Patchリクエストの共通レスポンス
+     */
+    private fun getGlobalPatchResponses() : List<ResponseMessage> = ArrayList()
+    /**
+     * Postリクエストの共通レスポンス
+     */
+    private fun getGlobalPostResponses() : List<ResponseMessage> = ArrayList()
+    /**
+     * Putリクエストの共通レスポンス
+     */
+    private fun getGlobalPutResponses() : List<ResponseMessage> = ArrayList()
+    /**
+     * Traceリクエストの共通レスポンス
+     */
+    private fun getGlobalTraceResponses() : List<ResponseMessage> = ArrayList()
+
+    /**
+     * 共通パラメータ
+     */
+    private fun getGlobalOperationParameters() : List<Parameter> = Lists.newArrayList(
+            ParameterBuilder()
+                    .name("globalParameter")
+                    .description("これは全API共通のパラメータです")
+                    .modelRef(ModelRef("string"))
+                    .parameterType("query")
+                    .required(true)
+                    .build()
+    )
+
+    /**
+     * APIの情報設定
+     */
+    private fun getApiInfo() : ApiInfo = ApiInfoBuilder()
+            .title("Swagger Example Title")
+            .description("Swagger Example Description")
+            .contact(Contact("Contact name" , "Contact Url" , "Contact Email") )
+            .license("License!" )
+            .licenseUrl("License Url!")
+            .termsOfServiceUrl("Terms Of Service Url!")
+            .version("1.0.0")
+            .build()
+
     @Bean
     fun configurateApiDocument(): Docket = Docket(DocumentationType.SWAGGER_2)
+
+            // ApiSelectorBuilderでAPI仕様書の対象とするAPIを選ぶ
             .select()
 
             // API仕様書に表示するAPIをアノテーションで指定
@@ -65,19 +138,59 @@ class SwaggerConfig {
             .apis(not(RequestHandlerSelectors.basePackage(IgnorePackageController::class.java.packageName)))
 
             // 全てのAPIを表示
-             .paths(PathSelectors.any())
+            // .paths(PathSelectors.any())
             // 全てのAPIを非表示
             // .paths(PathSelectors.none())
             // antPatternで該当のAPIを非表示
             // .paths(not(PathSelectors.ant("/sample/**")))
             // 特定のパスのAPIのみ非表示
-            // .paths(not(regex("/ignore/*")))
+            // ※「/error」はSpringが用意してくれてるデフォルトのエラー
+            // 　API仕様書にはいらない
+             .paths(not(regex("/error")))
 
             .build()
 
-            // APIのパスにコンテキストパスを含める
-            .pathMapping(contextPath)
+            // レスポンスのContent-Typeを指定する
+            // ※ほとんどの場合が「application/json」だとは思うけど
+            // 　個別に指定したい場合は定義しないほうがいいかも
+            .produces(newHashSet("application/json"))
 
+            // プロトコルを指定する
+            .protocols(newHashSet("http","https"))
+
+            // APIのパスにコンテキストパスを含める
+            // ※pathMappingで指定しなくてもBaseUrlにはちゃんとコンテキストパスがついている
+            // .pathMapping(contextPath)
+
+            // Getリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.GET, getGlobalGetResponses() )
+            // Deleteリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.DELETE , getGlobalDeleteResponses() )
+            // Headリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.HEAD , getGlobalHeadResponses() )
+            // Optionsリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.OPTIONS , getGlobalOptionsResponses() )
+            // Patchリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.PATCH , getGlobalPatchResponses() )
+            // Postリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.POST , getGlobalPostResponses() )
+            // Putリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.PUT , getGlobalPutResponses() )
+            // Traceリクエストの共通レスポンス
+            .globalResponseMessage( RequestMethod.TRACE , getGlobalTraceResponses() )
+
+            // デフォルトのレスポンスメッセージを使うかどうか
+            // trueにすると特に明記がなくてもレスポンス401,403,404等が出力される
+            // ※globalResponseMessageを使用するとtrueにしてもデフォルトのは消えるっぽい
+            .useDefaultResponseMessages(false )
+
+            // 全API共通パラメータ
+            .globalOperationParameters( getGlobalOperationParameters() )
+
+            // APIの情報設定
+            .apiInfo( getApiInfo() )
+
+            // TODO 以下あまり使い方よくわかってない
             .directModelSubstitute(LocalDate::class.java, String::class.java)
             .genericModelSubstitutes(ResponseEntity::class.java)
             .alternateTypeRules(
@@ -89,17 +202,6 @@ class SwaggerConfig {
                             typeResolver!!.resolve(WildcardType::class.java)
                     )
             )
-            .useDefaultResponseMessages(false)
-            .globalResponseMessage(
-                    RequestMethod.GET,
-                    Lists.newArrayList(
-                            ResponseMessageBuilder()
-                                    .code(500)
-                                    .message("500 message")
-                                    .responseModel(ModelRef("Error"))
-                                    .build()
-                    )
-            )
             .securitySchemes(Lists.newArrayList(ApiKey("mykey", "api_key", "header")))
             .securityContexts(Lists.newArrayList(SecurityContext.builder()
                     .securityReferences(defaultAuth())
@@ -107,16 +209,6 @@ class SwaggerConfig {
                     .build())
             )
             .enableUrlTemplating(true)
-            .globalOperationParameters(
-                    Lists.newArrayList(ParameterBuilder()
-                            .name("someGlobalParameter")
-                            .description("Description of someGlobalParameter")
-                            .modelRef(ModelRef("string"))
-                            .parameterType("query")
-                            .required(true)
-                            .build()
-                    )
-            )
             .tags(Tag("Pet Service", "All apis relating to pets"))
 
             // .additionalModels(typeResolver!!.resolve(AdditionalModel::class.java))
